ce585bd1b6a8c25fde22f2e3a0d012e46ca702b5
huangyp <harry@geekworm.com>
2021-06-23 20:35:46 +0800

    update

--- uninstall_x-c1.sh
+++ uninstall_x-c1.sh
@@ -2,6 +2,7 @@
 #remove x-c1 old installtion
 sudo sed -i '/x-c1/d' /etc/rc.local
 sudo sed -i '/xoff/d' ~/.bashrc
+sudo sed -i '/x-c1/d' ~/.bashrc
 
 sudo rm /usr/local/bin/x-c1softsd.sh -f
 sudo rm /etc/x-c1pwr.sh -f
