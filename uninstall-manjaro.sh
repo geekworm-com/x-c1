81a091b942a783c21f39dbbdb552b4362c52b954
Aleix Quintana Alsius <kinta@communia.org>
2022-03-02 23:51:15 +0100

    Update uninstall-manjaro.sh

--- uninstall-manjaro.sh
+++ uninstall-manjaro.sh
@@ -5,15 +5,18 @@
 sudo systemctl stop pigpiod
 sudo systemctl disable pigpiod
 
-sudo systemctl stop rc.local
-sudo systemctl disable rc.local
+sudo systemctl stop x-c1-pwr
+sudo systemctl stop x-c1-fan
+sudo systemctl disable x-c1-pwr
+sudo systemctl disable x-c1-fan
 
 sudo rm /lib/systemd/system/pigpiod.service -f
-sudo rm /lib/systemd/system/rc.local.service -f
+sudo rm /lib/systemd/system/x-c1-pwr.service -f
+sudo rm /lib/systemd/system/x-c1-fan.service -f
 
 #sudo rm /etc/profile.d/naspi.sh -f
-sudo rm /etc/rc.local
 sudo sed -i '/x-c1/d' ~/.bashrc
 
 sudo rm /usr/local/bin/x-c1-softsd.sh -f
+sudo rm /usr/local/bin/fan.py -f
 sudo rm /etc/x-c1pwr.sh -f
