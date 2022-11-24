14a6a464bd07c707847d4d080dd54805fa7ddb1c
huangyp <harry@geekworm.com>
2022-11-24 12:49:14 +0800

    add a fan-rpi.py (don't use pigpiod liberary to control pwm fan)

---
+++ fan-rpi.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+# DON'T use pigpiod liberary on 2022.11.24
+#import pigpio
+import RPi.GPIO as IO
+import time
+import subprocess
+
+servo = 18
+
+IO.setwarnings(False)
+IO.setmode (IO.BCM)
+IO.setup(servo,IO.OUT)
+fan = IO.PWM(servo,25000)
+fan.start(0)
+
+def get_temp():
+    output = subprocess.run(['vcgencmd', 'measure_temp'], capture_output=True)
+    temp_str = output.stdout.decode()
+    try:
+        return float(temp_str.split('=')[1].split('\'')[0])
+    except (IndexError, ValueError):
+        raise RuntimeError('Could not get temperature')
+
+while 1:
+    temp = get_temp()                        # Get the current CPU temperature
+    if temp > 70:                            # Check temperature threshhold, in degrees celcius
+        fan.ChangeDutyCycle(100)             # Set fan duty based on temperature, 100 is max speed and 0 is min speed or off.
+    elif temp > 60:
+        fan.ChangeDutyCycle(85)
+    elif temp > 50:
+        fan.ChangeDutyCycle(70)
+    elif temp > 40:
+        fan.ChangeDutyCycle(50)
+    elif temp > 32:
+        fan.ChangeDutyCycle(25)
+    elif temp > 25:
+        fan.ChangeDutyCycle(15)
+    else:
+        fan.ChangeDutyCycle(0)
+    time.sleep(5)                            # Sleep for 5 seconds
